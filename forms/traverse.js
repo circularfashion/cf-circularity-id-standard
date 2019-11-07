138833a7b769295cd703af9723b1f868ff12c5df
niko niko <niko@niko.io>
2019-11-07 17:55:32 +0100

    thinking

--- forms/traverse.js
+++ forms/traverse.js
@@ -5,11 +5,19 @@ const { matches } = require('z');
 const {
   get,
   keys,
+  map,
+  isArray,
 } = _;
 
 const objectId = 'ATTR.name';
 
 const traverse = (object, objectKey) => {
+  // objectKey could be optional, element, attr, ref,
+  // oneOrMore, zeroOrMore, list.choice.value
+
+  if (isArray(object)) return map(traverse(object));
+  // if has key element inject that
+
   id = get(objectId, object);
   ks = keys(object);
 };
