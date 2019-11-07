eae5b0007b9f86e1f0138f3125f9e3806890e931
niko niko <niko@niko.io>
2019-11-07 17:26:13 +0100

    some work

--- forms/index.js
+++ forms/index.js
@@ -1,8 +1,14 @@
 // following line generated with
 // xml2js.parseString(f, (e, r) => (d = r))
 // loaded to memory for now
-const bigData = require('./relaxJsObj.json').grammar;
+
+// const bigData = require('./relaxJsObj.json').grammar;
+const xml2js = require('xml2js');
+const fs = require('fs');
 const _ = require('lodash/fp');
+
+const f = fs.readFileSync('../schema/0.02/schema.rng', "utf8");
+
 
 const {
   map,
@@ -10,56 +16,64 @@ const {
   isArray,
   isObject,
   mapValues,
+  mapKeys,
   merge,
   omit,
   get,
+  uniq,
 } = _;
 
-const mapValues2 = mapValues.convert({cap: false})
-const map2 = map.convert({cap: false})
+const mapValues2 = mapValues.convert({cap: false});
+const mapKeys2 = mapKeys.convert({cap: false});
+const map2 = map.convert({cap: false});
 
-const traverseObject = (func, obj, kk, oo) => {
-  if (isArray(obj)) {
-    return map2((v, k) => {
-      func(obj, kk, oo);
-      return traverseObject(func, v, k, obj)
-    }, obj);
-  } else if (isObject(obj)) {
-    return mapValues2((v, k, o) => {
-      func(obj, kk, oo);
-      return traverseObject(func, v, k, o)
-    }, obj);
-  } else {
-    return func(obj, kk, oo);
-  }
-};
-
-console.log('hello');
-const elements = []
-
-const val = (v, k, o) => {
-  // console.log(v)
-  // console.log(k)
-  // console.log(o);
-  if (k === '$') {
-    const a = get('name', v);
-    if (a === 'material') {
-      console.log(v)
-      console.log(k)
-      console.log(o);
-      console.log('-----');
+const rest = bigData => {
+  console.log(bigData);
+  fs.writeFileSync('./relaxJsObj.json', JSON.stringify(bigData, null, 2));
+  const traverseObject = (func, obj, kk, oo) => {
+    if (isArray(obj)) {
+      return map2((v, k) => {
+        func(obj, kk, oo);
+        return traverseObject(func, v, k, obj)
+      }, obj);
+    } else if (isObject(obj)) {
+      return mapValues2((v, k, o) => {
+        func(obj, kk, oo);
+        return traverseObject(func, v, k, o)
+      }, obj);
+    } else {
+      return func(obj, kk, oo);
     }
-    elements.push(o);
+  };
+
+  console.log('hello');
+  const elements = []
+
+  const val = (v, k, o) => {
+    const y = get('ATTR.name', v);
+    console.log('--');
+    if (y && y === 'material') {
+      elements.push(v);
+      console.log(y)
+      console.log(k)
+    }
+    return v;
   }
-  return v;
+
+
+  const l = traverseObject(val, bigData.grammar.define)
+
+
+
+  const p = map(x => omit(['ATTR'], merge(x, {
+    name: get("ATTR.name", x),
+  })), uniq(elements));
+
+  console.log(uniq(p));
+  console.log('---------');
 }
 
-const l = traverseObject(val, bigData.define)
-
-const p = map(x => omit(['$'], merge(x, {
-  name: get("name", x['$']),
-})), elements);
-
-console.log(p)
-
-module.exports = p
+xml2js.parseStringPromise(f, {
+  attrkey: 'ATTR',
+  explicitArray: false,
+}).then(rest);
