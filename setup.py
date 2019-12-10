089a66f8fb3fe5b1d1eb83dfa626b82cc783f8c2
niko niko <niko@niko.io>
2019-12-10 14:23:07 +0100

    major shuffle of files

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(name='cf-circularity-id-standard',
       version='1.0',
-      description='Circular.Fashion circularity.ID Standard',
+      description='circular.fashion circularity.ID Standard',
       url='http://github.com/circularfashion/cf-circularity-id-standard',
       author='Circular.Fashion UG (haftungsbeschraenkt)',
       author_email='develop@circular.fashion',
       license='MIT',
       packages=['circularity_id_standard'],
+      package_dir={'circularity_id_standard': 'tools/circularity_id_standard'},
       zip_safe=False)
