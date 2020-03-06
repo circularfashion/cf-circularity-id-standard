cdcb93fd17d20fbb19889cbcab1f5f672e665201
niko <niko@niko.io>
2020-03-06 12:30:19 +0100

    updated schema 2.0

--- tools/circularity_id_standard/utils/xml.py
+++ tools/circularity_id_standard/utils/xml.py
@@ -9,11 +9,7 @@ from fnc import (
     compose,
 )
 
-<<<<<<< HEAD
-from xmltodict import ( 
-=======
 from xmltodict import (
->>>>>>> dev-1.01
     parse as xml_to_o_dict,
     unparse as dict_to_xml,
 )
